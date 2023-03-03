5078bd608e16add28047a7b72ca81350d79f560f
dlgnlwo3 <dlgnlwo3@naver.com>
2023-03-03 14:35:06 +0900

    워드 저장 완료

--- process/daum_search_process.py
+++ process/daum_search_process.py
@@ -20,6 +20,8 @@ from features.tistory_newspaper import TistoryNewsPaper
 from dtos.top_blog_detail_dto import *
 from config import *
 import pandas as pd
+import re
+from docx import Document
 
 
 class DaumSearch:
@@ -42,6 +44,26 @@ class DaumSearch:
     def blog_detail_to_excel(self, top_blog_detail_dtos):
         article_excel = os.path.join(self.guiDto.search_file_save_path, f"상위노출데이터 {self.run_time}.xlsx")
         pd.DataFrame.from_dict(top_blog_detail_dtos).to_excel(article_excel, index=False)
+        time.sleep(1)
+
+    # 워드 저장
+    def blog_detail_to_docx(self, article_title: str, article_text: str):
+
+        article_path = os.path.join(self.guiDto.search_file_save_path, f"글수집 {self.run_time}")
+
+        if os.path.isdir(article_path) == False:
+            os.mkdir(article_path)
+        else:
+            pass
+
+        article_docx = os.path.join(article_path, f"{article_title} {self.run_time[:-5]}.docx")
+
+        doc = Document()
+
+        doc.add_paragraph(article_text)
+
+        doc.save(article_docx)
+
         time.sleep(1)
 
     def search_top_blog(self, daum_keyword: str):
@@ -95,12 +117,30 @@ class DaumSearch:
             for blog in blog_list:
                 blog_date = blog.find_element(By.CSS_SELECTOR, "span[class*='date']").get_attribute("textContent")
                 blog_date_format = f"%Y.%m.%d"
-                blog_date = datetime.strptime(blog_date, blog_date_format)
+
+                try:
+                    blog_date = datetime.strptime(blog_date, blog_date_format)
+                except Exception as e:
+                    print(f"날짜 형식에 맞지 않습니다.")
+                    blog_date = datetime.now()
 
                 if search_date > blog_date:
-                    print("작업 진행")
+                    blog_url = blog.find_element(By.CSS_SELECTOR, 'a[class*="f_url"]').get_attribute("href")
+                    top_blog_detail_dto: TopBlogDetailDto = TistoryNewsPaper().get_article_from_blog_url(
+                        blog_url, daum_keyword
+                    )
+                    top_blog_detail_dict = top_blog_detail_dto.get_dict()
+                    article_text = top_blog_detail_dict["내용"]
+                    article_title = top_blog_detail_dict["제목"]
+                    article_title = re.sub('[\/:*?"<>|.]', "", article_title)
+
+                    # 워드 저장
+                    self.blog_detail_to_docx(article_title, article_text)
+
+                    search_blog_list.append(article_title)
+
                 else:
-                    print("넘김")
+                    print(f"({search_date}) 보다 이후에 작성된 글입니다. ({blog_date})")
                     continue
 
             if len(search_blog_list) >= self.guiDto.daum_search_count:
